90c83026620f779ccc99054d67a694c969412897
diegostanlev <162780518+diegostanlev@users.noreply.github.com>
2024-05-12 00:03:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -23,16 +23,14 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1608736275"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "postgres://koyeb-adm:fSIjTMsE4bu6@ep-red-king-a1x2vaqg.ap-southeast-1.pg.koyeb.app/koyebdb")
-DB_NAME = os.environ.get("DATABASE_NAME", "koyebdb")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://motof22932:@cluster0.asoxks3.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluser0")
 
 # force sub channel id, if you want enable force sub
 # give admin rights all fouth channels ( add yr bot into yr channels)
 
 FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1001947349335"))
 FORCE_SUB_CHANNEL_2 = int(os.environ.get("FORCE_SUB_CHANNEL_2", "-1002108419450"))
-FORCE_SUB_CHANNEL_3 = int(os.environ.get("FORCE_SUB_CHANNEL_3", "-1002108419450"))
-FORCE_SUB_CHANNEL_4 = int(os.environ.get("FORCE_SUB_CHANNEL_4", "-1002108419450"))
 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
